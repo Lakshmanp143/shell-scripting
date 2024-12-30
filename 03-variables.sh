262b9f8a61b6a237a08422a51530e4f97529fa53
Lakshmanpagala <laxman.gdn@gmail.com>
2024-12-30 11:16:57 +0530

    commit forwarding

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,7 +2,7 @@
 person1=laxman
 person2=ram
 
-echo "$person1:hi $person2"
-echo "$erson2:hello $person1"
+echo "$person1: hi $person2"
+echo "$person2: hello $person1"
 echo "$person1: good morning $person2"
 echo "$person2: very good morning $person1"
