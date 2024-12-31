f726a2fba9ef42e70978aad81f503753e2228d2c
Lakshmanpagala <laxman.gdn@gmail.com>
2024-12-31 15:28:20 +0530

    datatype script

---
+++ Special-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "All variables passed: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Home directory of present user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Process id of present script: $$"
+sleep 10
+echo "Process id of last command: $!"
