40414e1752c12348c1b609d77adb745e4ff468c8
Lakshmanpagala <laxman.gdn@gmail.com>
2024-12-31 14:51:48 +0530

    datatype script

--- datatype.sh
+++ datatype.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # Author: laxman
 # Practicing the shell-scripting for automating the usual work
-
-set -x
-set -e
-
-
 num1=$1
 num2=$2
 TIMESTAMP=$(date +%F)
@@ -13,6 +8,4 @@ SUM=$((num1+num2))
 echo "Script executed at: ${TIMESTAMP}"
 echo "Sum of num1 and num2 is: ${SUM}"
 
-pwd
- 
-ls -ltr
+
