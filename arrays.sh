9dd5c452d4a1f0699ab7935d36b1f93ee7b4be54
Lakshmanpagala <laxman.gdn@gmail.com>
2024-12-31 15:10:38 +0530

    datatype script

--- arrays.sh
+++ arrays.sh
@@ -2,6 +2,6 @@
 MOVIES=("maghadeera" "rrr" "bahibal")
 # Index start from 0 , size is 3
 echo "First movie: ${MOVIES[0]"
-echo "First movie: ${MOVIES[1]"
-echo "Second movie: ${MOVIES[2]"
+echo "Second movie: ${MOVIES[1]"
+echo "Third movie: ${MOVIES[2]"
 echo "All movies: ${MOVIES[@]"
