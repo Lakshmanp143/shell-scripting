b6a52381f36cf96e7303ad8189dec53feb1d9b62
Lakshmanpagala <laxman.gdn@gmail.com>
2024-12-31 08:44:27 +0530

    myqsl

--- install-package.sh
+++ install-package.sh
@@ -5,7 +5,8 @@ if [ $USERID -ne 0 ]; then
     echo "you need to be root user execute this script"
     exit 1
 fi
-dnf list installed mysql
+
+dnf list installed mysql -y
 if [ $? -ne 0 ]; then
 
     dnf install mysql -y
