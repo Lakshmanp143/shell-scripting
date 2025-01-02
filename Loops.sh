c3db0d9e07601ef1bdb9931c66bae81d42b7c4b0
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-02 19:14:55 +0530

    loops-script

--- Loops.sh
+++ Loops.sh
@@ -11,17 +11,17 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2.....$R FAILURE $N"
+        echo -e "$2 ..... $R FAILURE $N"
         exit 1
     else 
-        echo -e "$2.....$G success $N"
+        echo -e "$2 ..... $G success $N"
     fi
 }
 
-echo " Script started executing at: $TIMESTAMP"  &>>$LOG_FILE_NAME
+echo "Script started executing at: $TIMESTAMP"  &>>$LOG_FILE_NAME
 
 if [ $USERID -ne 0 ]; then
-    echo " You need to be root user to execute thi sscript"
+    echo " You need to be root user to execute this script"
     exit 1
 fi
 
