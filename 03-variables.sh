c7d559f59b45894500d225eb0b18b8643cb3e6e9
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-15 16:39:47 +0530

    shell script

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+PERSON1=laxman
+PERSON2=ram
+
+echo "$PERSON1: hello $PERSON2"
+echo "$PERSON2: hello $PERSON1"
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: doing well what about you $PERSON1"
