195f40caecbe52e85e7093a12eaadaa49163acba
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-15 18:40:36 +0530

    shell script

---
+++ 10-install_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: You need to be root user to execute this script"
+    exit 1
+fi
+
+dnf install list mysql
+if [ $? -ne 0 ]
+then
+    echo "Mysql not intalled, installing ....wait!!!!"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "mysql installing SUCCESS"
+    else
+        echo "mysql installing FAILURE"
+    fi
+else
+    echo "mysql already installed ...SKIPPING"
+fi
