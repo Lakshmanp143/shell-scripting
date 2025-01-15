69921e46dc4348130dc3c336a6220a94314c7713
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-15 17:44:31 +0530

    shell script

---
+++ 08-special_variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+all variables passed:$@
+number of variables passed:$#
+present script name:$0
+process id od present script:$$
+ls -ltra
+process id of previous command:$!
+present working directory:$PWD 
+home directory of current user:$HOME 
+which user execute this script:$USER 
