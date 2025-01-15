6483722e566101d00da32a0f7aa83161838f4a97
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-15 16:55:46 +0530

    shell script

--- 06-variable.sh
+++ 06-variable.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo "Please enter your USERNAME:: "
+read -s USERNAME
+
+echo "username:: $USERNAME"
+
+echo "please enter your PASSWORD:: "
+read -s password
+echo "password:: $PASSWORD"
